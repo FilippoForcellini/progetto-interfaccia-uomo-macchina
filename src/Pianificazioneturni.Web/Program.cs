2a2f929339c1f0e86f43f8942c26bbdbd659b201
gianninimatteo <matteogiannini294@gmail.com>
2025-12-15 14:46:50 +0100

    CIAO

--- src/Pianificazioneturni.Web/Program.cs
+++ src/Pianificazioneturni.Web/Program.cs
@@ -17,7 +17,7 @@ namespace Pianificazioneturni.Web
                     webBuilder.ConfigureKestrel(kestrel =>
                     {
                         kestrel.AddServerHeader = false; // OWASP: Remove Kestrel response header 
-                    }); //CIAO
+                    });
 
                     webBuilder.UseStartup<Startup>();
                 });
