b163de42154765148108cf27d83459d0556b4b3b
gianninimatteo <matteogiannini294@gmail.com>
2026-01-09 23:49:15 +0100

    modifiche 9/01/2026

---
+++ src/Pianificazioneturni.Web/Features/Home/HomeController.Pages.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pianificazioneturni.Web.Features.Home
+{
+    public partial class HomeController : Controller
+    {
+        public virtual IActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual IActionResult GestioneNavi()
+        {
+            return View();
+        }
+    }
+}
